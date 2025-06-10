9870852b6380cb3d59aab877b0d308c7a082a1e6
Taylor Vann <brian.t.vann@gmail.com>
2025-06-09 19:36:38 -0700

    add action kinds

--- type_flyweight/src/ip_address_rate_limits.rs
+++ type_flyweight/src/ip_address_rate_limits.rs
@@ -2,6 +2,13 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, PartialEq, Serialize, Deserialize, Debug)]
+pub struct IpAddressActionKind {
+    pub id: u64,
+    pub kind: String,
+    pub deleted_at: Option<u64>,
+}
+
+#[derive(Clone, PartialEq, Serialize, Deserialize, Debug)]
 pub struct IpAddressRateLimit {
     pub ip_address: String,
     pub kind_id: u64,
--- type_flyweight/src/people_action_rate_limits.rs
+++ type_flyweight/src/people_action_rate_limits.rs
@@ -2,6 +2,13 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, PartialEq, Serialize, Deserialize, Debug)]
+pub struct ActionKind {
+    pub id: u64,
+    pub kind: String,
+    pub deleted_at: Option<u64>,
+}
+
+#[derive(Clone, PartialEq, Serialize, Deserialize, Debug)]
 pub struct PeopleActionRateLimit {
     pub people_id: u64,
     pub kind_id: u64,
